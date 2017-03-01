3a81f10def642e7d0c594855161fde524c160fb3
Hemant Kumar Dindi <hemantdindi@users.noreply.github.com>
2017-03-01 22:39:49 +0530

    Update host-ambari.sh

--- setup/host-ambari.sh
+++ setup/host-ambari.sh
@@ -44,33 +44,33 @@ wget -nv https://jdbc.postgresql.org/download/postgresql-9.4.1212.jre6.jar -O /u
 ambari-server setup --jdbc-db=postgres --jdbc-driver=/usr/share/java/postgresql-jdbc.jar
 yum install ambari-agent -y
 sed -i 's/hostname=localhost/'hostname="$HOSTNAME"'/g' /etc/ambari-agent/conf/ambari-agent.ini
-#export realm=`hostname -d`
-#export REALM="${realm^^}"
-#export KDC_HOST=`hostname -f`
-#yum -y install krb5-server krb5-libs krb5-workstation
-#cp /etc/krb5.conf .
-#mv /etc/krb5.conf /tmp/
-#sed -i "s/EXAMPLE.COM/$REALM/g" krb5.conf
-#sed -i "s/example.com/$realm/g" krb5.conf
-#sed -i "s/kerberos.$realm/$KDC_HOST/g" krb5.conf
-#sed -i '2,$s/#//' krb5.conf
-#cp krb5.conf /etc/krb5.conf
-#kdb5_util create -s -P hadoop
-#service krb5kdc start
-#service kadmin start
-#systemctl enable krb5kdc
-#systemctl enable kadmin
-#kadmin.local -q "addprinc -pw hadoop admin/admin"
-#sed -i "s/EXAMPLE.COM/$REALM/g" /var/kerberos/krb5kdc/kadm5.acl
-#service krb5kdc restart
-#service kadmin restart
-#service postgresql restart
-#su postgres -c psql <<EOF
-#\x
-#create database hivedb;
-#create user hiveuser with password 'hadoop';
-#grant all privileges on database hivedb to hiveuser;
-#EOF
-#echo "host all all 0.0.0.0/0 trust" >> /var/lib/pgsql/data/pg_hba.conf
-#service postgresql restart
-#ambari-server restart
+export realm=`hostname -d`
+export REALM="${realm^^}"
+export KDC_HOST=`hostname -f`
+yum -y install krb5-server krb5-libs krb5-workstation
+cp /etc/krb5.conf .
+mv /etc/krb5.conf /tmp/
+sed -i "s/EXAMPLE.COM/$REALM/g" krb5.conf
+sed -i "s/example.com/$realm/g" krb5.conf
+sed -i "s/kerberos.$realm/$KDC_HOST/g" krb5.conf
+sed -i '2,$s/#//' krb5.conf
+cp krb5.conf /etc/krb5.conf
+kdb5_util create -s -P hadoop
+service krb5kdc start
+service kadmin start
+systemctl enable krb5kdc
+systemctl enable kadmin
+kadmin.local -q "addprinc -pw hadoop admin/admin"
+sed -i "s/EXAMPLE.COM/$REALM/g" /var/kerberos/krb5kdc/kadm5.acl
+service krb5kdc restart
+service kadmin restart
+service postgresql restart
+su postgres -c psql <<EOF
+\x
+create database hivedb;
+create user hiveuser with password 'hadoop';
+grant all privileges on database hivedb to hiveuser;
+EOF
+echo "host all all 0.0.0.0/0 trust" >> /var/lib/pgsql/data/pg_hba.conf
+service postgresql restart
+ambari-server restart
