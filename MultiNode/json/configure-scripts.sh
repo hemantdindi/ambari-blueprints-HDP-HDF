f7f2753891a6258fa8acbdd0768af3eb1ae707f7
Hemant Kumar Dindi <hemantkumar.dindi@gmail.com>
2017-03-06 20:27:05 +0530

    Added multinode

---
+++ MultiNode/json/configure-scripts.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export realm=`hostname -d`
+export REALM="${realm^^}"
+sed -i "s/blueprintclusterhost/$HOSTNAME/g" cluster_configuration.json
+sed -i "s/SPECIFYREALM/$REALM/g" cluster_configuration.json
+sed -i "s/SPECIFYDOMAIN/$realm/g" cluster_configuration.json
+sed -i "s/blueprintclusterhost/$HOSTNAME/g" hostmapping.json
+sed -i "s/blueprintclusterhost/$HOSTNAME/g" registerBluePrint.sh
