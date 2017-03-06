ab15aa89acae50d75142b432f7fad341588c18f2
Hemant Kumar Dindi <hemantkumar.dindi@gmail.com>
2017-03-06 21:48:29 +0530

    updated scripts

--- MultiNode/json/configure-scripts.sh
+++ MultiNode/json/configure-scripts.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-export realm=`hostname -d`
-export REALM="${realm^^}"
-sed -i "s/blueprintclusterhost/$HOSTNAME/g" cluster_configuration.json
-sed -i "s/SPECIFYREALM/$REALM/g" cluster_configuration.json
-sed -i "s/SPECIFYDOMAIN/$realm/g" cluster_configuration.json
-sed -i "s/blueprintclusterhost/$HOSTNAME/g" hostmapping.json
-sed -i "s/blueprintclusterhost/$HOSTNAME/g" registerBluePrint.sh
+export HOSTNAME1=node01.hadoophdp.com
+export HOSTNAME2=node02.hadoophdp.com
+export HOSTNAME3=node03.hadoophdp.com
+export AMBARISERVER=node01.hadoophdp.com
+sed -i "s/HOSTNAME1/$HOSTNAME1/g" cluster_configuration.json
+sed -i "s/HOSTNAME1/$HOSTNAME1/g" hostmapping.json
+sed -i "s/HOSTNAME2/$HOSTNAME2/g" hostmapping.json
+sed -i "s/HOSTNAME3/$HOSTNAME3/g" hostmapping.json
+sed -i "s/AMBARISERVER/$AMBARISERVER/g" registerBluePrint.sh
