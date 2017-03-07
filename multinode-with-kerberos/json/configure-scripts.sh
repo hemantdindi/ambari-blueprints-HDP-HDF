903a9e5483f6908271dadb3387ebf3bb5e4df1d2
Hemant Kumar Dindi <hemantdindi@users.noreply.github.com>
2017-03-07 21:31:14 +0530

    updated KDC variables

--- multinode-with-kerberos/json/configure-scripts.sh
+++ multinode-with-kerberos/json/configure-scripts.sh
@@ -3,6 +3,8 @@ export HOSTNAME1=node01.hadoophdp.com
 export HOSTNAME2=node02.hadoophdp.com
 export HOSTNAME3=node03.hadoophdp.com
 export AMBARISERVER=node01.hadoophdp.com
+export KDCADMINHOST=node01.hadoophdp.com
+export KDCHOST=node01.hadoophdp.com
 sed -i "s/HOSTNAME1/$HOSTNAME1/g" cluster_configuration.json
 sed -i "s/HOSTNAME1/$HOSTNAME1/g" hostmapping.json
 sed -i "s/HOSTNAME2/$HOSTNAME2/g" hostmapping.json
