57c6622a8b284076399bf060814b9dc8d14886b4
Hemant Kumar Dindi <hemantdindi@users.noreply.github.com>
2018-06-28 07:30:17 +0530

    Update configure-scripts.sh

--- singlenode-with-kerberos/json/configure-scripts.sh
+++ singlenode-with-kerberos/json/configure-scripts.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export realm=`hostname -d`
 export REALM="${realm^^}"
+export HOSTNAME=`hostname -f`
 sed -i "s/blueprintclusterhost/$HOSTNAME/g" cluster_configuration.json
 sed -i "s/SPECIFYREALM/$REALM/g" cluster_configuration.json
 sed -i "s/SPECIFYDOMAIN/$realm/g" cluster_configuration.json
